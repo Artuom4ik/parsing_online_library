c37243921b0d85551df1f5bb31746ef365fb6329
Artuom4ik <masterskayaigr123@gmail.com>
2023-07-19 23:25:41 +0300

    download books with titles.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
 
 
 def check_for_redirect(response):
@@ -9,21 +10,31 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt( url , filename, folder='books/' ):
-    filename = sanitize_filename(filename)
-    return os.path.join(folder, filename)    
+def download_txt(url, filename, num_book, folder='books/'):
+    filename = f'{num_book}.{sanitize_filename(filename)}.txt'
+    return os.path.join(folder, filename)
+
+
+def get_title_author_book(num_book):
+    url = f'https://tululu.org/b{num_book}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup.find('body').find('table').find('div', id='content').find('h1').text.split('::')
 
 
 if __name__ == '__main__':
     os.makedirs('books', exist_ok=True)
-    for num in range(10):
-        url = f"https://tululu.org/txt.php?id={num}"
-        filename = f'id{num + 1}.txt'
+    for num_book in range(1, 11):
+        url = f"https://tululu.org/txt.php?id={num_book}"
         response = requests.get(url)
         response.raise_for_status()
         try:
             check_for_redirect(response)
-            with open(f'books/{filename}', 'wb') as file:
+            filename, author = get_title_author_book(num_book)
+            filename = filename.strip()
+            directory = download_txt(url, filename, num_book)
+            with open(directory, 'wb') as file:
                 file.write(response.content)
         except requests.HTTPError:
             continue
