4e275d3ce7b6bac2962798be49a0a793da408581
Artuom4ik <masterskayaigr123@gmail.com>
2023-07-19 19:36:45 +0300

    add function check_for_redirect.

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import os
 import requests
 
 
-os.makedirs('books', exist_ok=True)
-for num in range(10):
-    filename = f'id{num}.txt'
-    url = f"https://tululu.org/txt.php?id={num}"
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(f'books/{filename}', 'wb') as file:
-        file.write(response.content)
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
+
+if __name__ == '__main__':
+    os.makedirs('books', exist_ok=True)
+    for num in range(10):
+        filename = f'id{num + 1}.txt'
+        url = f"https://tululu.org/txt.php?id={num}"
+        response = requests.get(url)
+        response.raise_for_status()
+        try:
+            check_for_redirect(response)
+            with open(f'books/{filename}', 'wb') as file:
+                file.write(response.content)
+        except requests.HTTPError:
+            continue
