7231f53c23c833ad55dc5d8fdf7281c47f04af17
Artuom4ik <masterskayaigr123@gmail.com>
2023-07-19 21:04:28 +0300

    add function download_txt.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import requests
+from pathvalidate import sanitize_filename
 
 
 def check_for_redirect(response):
@@ -8,11 +9,16 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
+def download_txt( url , filename, folder='books/' ):
+    filename = sanitize_filename(filename)
+    return os.path.join(folder, filename)    
+
+
 if __name__ == '__main__':
     os.makedirs('books', exist_ok=True)
     for num in range(10):
-        filename = f'id{num + 1}.txt'
         url = f"https://tululu.org/txt.php?id={num}"
+        filename = f'id{num + 1}.txt'
         response = requests.get(url)
         response.raise_for_status()
         try:
