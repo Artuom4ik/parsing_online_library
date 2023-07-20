d3f827712979594166284d9cbfc5b1055a92ea8a
Artuom4ik <masterskayaigr123@gmail.com>
2023-07-20 20:17:29 +0300

    download book covers.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
 def check_for_redirect(response):
@@ -12,7 +13,18 @@ def check_for_redirect(response):
 
 def download_txt(url, filename, num_book, folder='books/'):
     filename = f'{num_book}.{sanitize_filename(filename)}.txt'
-    return os.path.join(folder, filename)
+    file_path = os.path.join(folder, filename)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+
+def download_img(img_url, filename, folder='images/'):
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(img_url)
+    response.raise_for_status()
+    file_path = os.path.join(folder, filename)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
 
 
 def get_title_author_book(num_book):
@@ -20,21 +32,35 @@ def get_title_author_book(num_book):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    return soup.find('body').find('table').find('div', id='content').find('h1').text.split('::')
+    return soup.find('h1').text.split('::')
 
 
 if __name__ == '__main__':
-    os.makedirs('books', exist_ok=True)
-    for num_book in range(1, 11):
-        url = f"https://tululu.org/txt.php?id={num_book}"
+    # os.makedirs('books', exist_ok=True)
+    # for num_book in range(1, 11):
+    #     url = f"https://tululu.org/txt.php?id={num_book}"
+    #     response = requests.get(url)
+    #     response.raise_for_status()
+    #     try:
+    #         check_for_redirect(response)
+    #     except requests.HTTPError:
+    #         continue
+    #     filename, author = get_title_author_book(num_book)
+    #     filename = filename.strip()
+    #     author = author.strip()
+    #     download_txt(url, filename, num_book)
+    for i in range(1, 11):
+        url = f'https://tululu.org/b{i}/'
         response = requests.get(url)
         response.raise_for_status()
         try:
             check_for_redirect(response)
-            filename, author = get_title_author_book(num_book)
-            filename = filename.strip()
-            directory = download_txt(url, filename, num_book)
-            with open(directory, 'wb') as file:
-                file.write(response.content)
-        except requests.HTTPError:
+        except requests.HTTPError: 
             continue
+        soup = BeautifulSoup(response.text, 'lxml')
+        relative_url = soup.find('div', class_='bookimage').find('img')['src']
+        # title, author = soup.find('h1').text.split('::')
+        img_url = urljoin('https://tululu.org/', relative_url)
+        img_name = relative_url.split('/')[-1]
+        download_img(img_url, img_name)
+        
