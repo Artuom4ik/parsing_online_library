00e9d3b133cbbe161b4a62b761693f5cdb68fb56
Artuom4ik <masterskayaigr123@gmail.com>
2023-07-27 18:57:05 +0300

    add function get_range.

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
+def get_range():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('start_id', nargs='?', type=int, default=1)
+    parser.add_argument('end_id', nargs='?', type=int, default=10)
+    args = parser.parse_args()
+    return args.start_id, args.end_id
+
+
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
@@ -35,21 +43,25 @@ def download_img(img_url, filename, folder='images/'):
         file.write(response.content)
 
 
-def parse_book_page(response, num_book):
-    soup = BeautifulSoup(response.text, 'lxml')
+def parse_book_page(page, num_book):
+    soup = BeautifulSoup(page, 'lxml')
     relative_url = soup.find('div', class_='bookimage').find('img')['src']
     img_url = urljoin('https://tululu.org/', relative_url)
     title, author = get_title_author_book(num_book)
+    genre_tags = soup.find('span', class_='d_book').find_all('a')
+    comments_tags = soup.find_all('div', class_='texts')
     genres = [genre.text
-            for genre in soup.find('span', class_='d_book').find_all('a')]
+              for genre in genre_tags]
     comments = [comment.find('span').text
-            for comment in soup.find_all('div', class_='texts')]
+                for comment in comments_tags]
 
-    return {'title': title.strip(),
-            'author': author.strip(),
-            'img_url': img_url,
-            'genres': genres,
-            'comments': comments}
+    return {
+        'title': title.strip(),
+        'author': author.strip(),
+        'img_url': img_url,
+        'genres': genres,
+        'comments': comments
+    }
 
 
 def get_title_author_book(num_book):
@@ -61,12 +73,9 @@ def get_title_author_book(num_book):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('start_id', nargs='?', type=int, default=1)
-    parser.add_argument('end_id', nargs='?', type=int, default=10)
-    args = parser.parse_args()
+    start_id, end_id = get_range()    
 
-    for num_book in range(args.start_id, args.end_id + 1):
+    for num_book in range(start_id, end_id + 1):
         url = f'https://tululu.org/b{num_book}/'
         response = requests.get(url)
         response.raise_for_status()
@@ -74,6 +83,6 @@ if __name__ == '__main__':
             check_for_redirect(response)
         except requests.HTTPError:
             continue
-        book_description = parse_book_page(response, num_book)
-        filename = book_description["title"]
-        download_txt(filename, num_book)
+        book_description = parse_book_page(response.text, num_book)
+        name_book = book_description["title"]
+        download_txt(name_book, num_book)
